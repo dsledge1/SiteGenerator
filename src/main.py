5d30ffb2ed2e13ca2357659475116773d6b2366f
David Sledge <david.sledge1@gmailcom>
2025-08-12 15:37:48 -0400

    minor fixes to main

--- src/main.py
+++ src/main.py
@@ -6,34 +6,38 @@ from htmlnode import *
 from splitnode import *
 from textnode import *
 
+print("main.py is running!")
+
 def rec_copy(source, dest):
     pwd = "/home/dsledge1/workspace/github.com/dsledge1/SiteGenerator"
     s = os.path.join(pwd, source)
     d = os.path.join(pwd, dest)
-    print(f"copying from {s} to {d}")
-    print(f"items to be copied: {os.listdir(s)}")
+    #print(f"copying from {s} to {d}")
+    #print(f"items to be copied: {os.listdir(s)}")
+    if not os.path.exists(d):
+        os.makedirs(d)
     if os.path.exists(d):
         if os.listdir(d):
-            print(f"Destination is not empty, deleting destination tree at {d}")
+            #print(f"Destination is not empty, deleting destination tree at {d}")
             shutil.rmtree(d)
-            print(f"making fresh directory {d}")
+            #print(f"making fresh directory {d}")
             os.mkdir(d)
     for item in os.listdir(s):
         i = os.path.join(s, item)
-        print(f"processing {i}")
+        #print(f"processing {i}")
         if os.path.isdir(i):
-            print(f"{item} is a directory")
+            #print(f"{item} is a directory")
             new_dest = os.path.join(d, item)
-            print(f"{new_dest} is the new correponding target directory")
+            #print(f"{new_dest} is the new correponding target directory")
             if not os.path.exists(new_dest):
-                print(f"{new_dest} does not exist, creating directory")
+                #print(f"{new_dest} does not exist, creating directory")
                 os.makedirs(new_dest)
-            print(f"Recursively copying files inside {item}")
+            #print(f"Recursively copying files inside {item}")
             rec_copy(os.path.join(s, item), new_dest)
         elif not os.path.isdir(i):
-            print(f"Copying {item} from {s} to {d}")
+            #print(f"Copying {item} from {s} to {d}")
             shutil.copy(os.path.join(s,item), d)
-            print(f"{item} copied successfully")
+            #print(f"{item} copied successfully")
 
 def extract_title(markdown):
     string = markdown.strip()
@@ -45,7 +49,7 @@ def extract_title(markdown):
             raise Exception("No header found")
 
 def generate_page(from_path, template_path, dest_path):
-    print(f"Generating page from {from_path} using template {template_path} to {dest_path}")
+    #print(f"Generating page from {from_path} using template {template_path} to {dest_path}")
     md = open(from_path, "r").read()
     template = open(template_path, "r").read()
     html = markdown_to_html_node(md).to_html()
@@ -53,6 +57,9 @@ def generate_page(from_path, template_path, dest_path):
     template = template.replace("{{ Title }}", f"{title}")
     template = template.replace("{{ Content }}", f"{html}")
     if not os.path.exists(dest_path):
+        os.makedirs(dest_path)
+    if not os.path.isdir(dest_path):
+        os.remove(dest_path)
         os.makedirs(dest_path)
     dest_file = os.path.join(dest_path, "index.html")
     with open(dest_file, "w") as f:
